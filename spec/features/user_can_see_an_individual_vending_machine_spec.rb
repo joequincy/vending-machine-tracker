deb8c34ad124fc1179936e1a6ec4781a8b557a66
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-04-05 10:16:30 -0600

    Add test for displaying snacks on machine show page

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -1,12 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'When a user visits a vending machine show page', type: :feature do
-  scenario 'they see the location of that machine' do
-    owner = Owner.create(name: "Sam's Snacks")
-    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+  before(:each) do
+    @owner = Owner.create(name: "Sam's Snacks")
+    @dons  = @owner.machines.create(location: "Don's Mixed Drinks")
+  end
 
-    visit machine_path(dons)
+  scenario 'they see the location of that machine' do
+    visit machine_path(@dons)
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
+  end
+
+  scenario 'they see a list of snacks in the machine, and their price' do
+    snack_1 = @dons.snacks.create(name: "Fruit Roll-Ups", price: 0.50)
+    snack_2 = @dons.snacks.create(name: "Chex Trail Mix", price: 1.50)
+    visit machine_path(@dons)
+
+    expect(page).to have_content("#{snack_1.name}: $#{snack_1.price}")
+    expect(page).to have_content("#{snack_2.name}: $#{snack_2.price}")
   end
 end

8f95e8e9665ef511cae30115b3ba0799ceb19282
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-04-05 10:27:09 -0600

    Add test for displaying average price

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -4,20 +4,21 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
   before(:each) do
     @owner = Owner.create(name: "Sam's Snacks")
     @dons  = @owner.machines.create(location: "Don's Mixed Drinks")
+    @snack_1 = @dons.snacks.create(name: "Fruit Roll-Ups", price: 0.50)
+    @snack_2 = @dons.snacks.create(name: "Chex Trail Mix", price: 1.50)
+    visit machine_path(@dons)
   end
 
   scenario 'they see the location of that machine' do
-    visit machine_path(@dons)
-
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
 
   scenario 'they see a list of snacks in the machine, and their price' do
-    snack_1 = @dons.snacks.create(name: "Fruit Roll-Ups", price: 0.50)
-    snack_2 = @dons.snacks.create(name: "Chex Trail Mix", price: 1.50)
-    visit machine_path(@dons)
+    expect(page).to have_content("#{@snack_1.name}: $0.50")
+    expect(page).to have_content("#{@snack_2.name}: $1.50")
+  end
 
-    expect(page).to have_content("#{snack_1.name}: $#{snack_1.price}")
-    expect(page).to have_content("#{snack_2.name}: $#{snack_2.price}")
+  scenario 'they see the average price of snacks in machine' do
+    expect(page).to have_content("Average Price: $1.00")
   end
 end

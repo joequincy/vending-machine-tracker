ce2ef8fb392a5e9c65faed12bc1ae9c216f6dcb5
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-04-05 10:39:34 -0600

    Add helper method to display prices correctly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :float_to_dollars
+
+  def float_to_dollars(float)
+    dollars, cents = float.divmod(1)
+    cents = (cents * 100).to_i.to_s.ljust(2, '0')
+    currency = "#{dollars.to_i}.#{cents}"
+  end
 end
